7abfc0328a239f751bbcc36fbab7ff84b38cbdc7
Diogo Bastos <diogo.bastos@neovation.local>
2017-02-21 11:20:03 -0600

    Add nav search support

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -14,7 +14,7 @@ var React = require('react');
 var WeatherMessage = ({temp, location}) =>{
 
   return (
-    <h3 className="text-center">It's it {temp} in {location}.</h3>
+    <h3 className="text-center">It's it {temp} °C in {location}.</h3>
   );
 }
 
